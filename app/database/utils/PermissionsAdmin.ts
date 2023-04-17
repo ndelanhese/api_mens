1514d0537117dd95b856957c1bdfe447f22c5856
ndelanhese <nnathanh3@gmail.com>
2023-04-17 19:55:22 -0300

    fix(permissions): fixed permissions seed

--- app/database/utils/PermissionsAdmin.ts
+++ app/database/utils/PermissionsAdmin.ts
@@ -1,7 +1,7 @@
 /* eslint-disable no-plusplus */
 export const permissionsAdmin = () => {
   const permissions = [];
-  for (let i = 1; i <= 18; i++) {
+  for (let i = 1; i <= 16; i++) {
     permissions.push({
       permission_id: i,
       user_id: 1,
