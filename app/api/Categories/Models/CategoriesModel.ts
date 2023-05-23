792bcf9f696ab901e740ae8208ebfb3774ce427c
ndelanhese <nnathanh3@gmail.com>
2023-05-23 08:45:56 -0300

    fix(categories): changed const name brands -> categories

--- app/api/Categories/Models/CategoriesModel.ts
+++ app/api/Categories/Models/CategoriesModel.ts
@@ -10,11 +10,11 @@ export default class CategoriesModel {
     }
   }
 
-  public async getCategory(brand_id: number) {
+  public async getCategory(category_id: number) {
     try {
-      const brand = await categoriesModel.findByPk(brand_id);
-      if (!brand) throw new HttpError(404, 'Categoria não encontrada.');
-      return brand;
+      const category = await categoriesModel.findByPk(category_id);
+      if (!category) throw new HttpError(404, 'Categoria não encontrada.');
+      return category;
     } catch (error) {
       if (error instanceof HttpError)
         throw new HttpError(error.statusCode, error.message, error);
