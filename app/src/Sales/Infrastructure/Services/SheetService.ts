ff4a943a539c1f8672c8eecb300011dc58176374
ndelanhese <nnathanh3@gmail.com>
2023-05-24 18:54:25 -0300

    feat(sales): added new parameters on sales export

--- app/src/Sales/Infrastructure/Services/SheetService.ts
+++ app/src/Sales/Infrastructure/Services/SheetService.ts
@@ -8,7 +8,9 @@ export default class SheetService {
   public dataToSheet(products: Sale[]): Buffer {
     const workSheetName = 'SheetJS';
     const rowsData = products.map(item => ({
-      DATA: getDateString(item.getDate()),
+      CÓDIGO: item.getId(),
+      DATA: this.prepareDate(item.getDate()),
+      CLIENTE: this.prepareClienteName(item.getCustomer().getName()),
       //TODO -> Colocar os outros dados de data
     }));
     const workSheet: xlsx.WorkSheet = xlsx.utils.json_to_sheet(rowsData);
@@ -16,5 +18,26 @@ export default class SheetService {
     const workBook: xlsx.WorkBook = xlsx.utils.book_new();
     xlsx.utils.book_append_sheet(workBook, workSheetPrepared, workSheetName);
     return xlsx.write(workBook, { type: 'buffer', bookType: 'xlsx' });
+  }
+
+  private prepareDate(date: Date) {
+    return getDateString(date);
+  }
+  private prepareClienteName(customerName: string) {
+    const names: string[] = customerName.split(' ');
+
+    if (names.length >= 2) {
+      const firstName: string = names[0];
+      const lastName: string = names[names.length - 1];
+
+      const initials: string = names
+        .slice(1, -1)
+        .map((nome: string) => `${nome.charAt(0).toUpperCase()}.`)
+        .join(' ');
+
+      return `${firstName} ${initials} ${lastName}`;
+    }
+
+    return customerName;
   }
 }
