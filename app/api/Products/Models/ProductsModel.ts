b8a9c3abf5ac3eb063eeb1310cffc7256a563a22
ndelanhese <nnathanh3@gmail.com>
2023-05-25 21:59:31 -0300

    fix(products): fixed products list association

--- app/api/Products/Models/ProductsModel.ts
+++ app/api/Products/Models/ProductsModel.ts
@@ -1,13 +1,33 @@
+import BrandsModel from '@db-models/BrandsModel';
+import categoriesModel from '@db-models/CategoriesModel';
 import productsModel from '@db-models/ProductsModel';
+import SuppliersModel from '@db-models/SuppliersModel';
 import HttpError from '@exceptions/HttpError';
 
 export default class ProductsModel {
   public async getProducts(order: string, direction: string) {
+    //TODO -> Adicionar os include individual (category, brand and supplier)
     try {
       return await productsModel.findAll({
         order: [[order, direction]],
         attributes: { exclude: ['category_id', 'brand_id', 'supplier_id'] },
-        include: { all: true },
+        include: [
+          {
+            model: categoriesModel,
+            as: 'category',
+            attributes: ['id', 'name'],
+          },
+          {
+            model: BrandsModel,
+            as: 'brand',
+            attributes: ['id', 'name'],
+          },
+          {
+            model: SuppliersModel,
+            as: 'supplier',
+            attributes: ['id', 'contact_name', 'corporate_name', 'cnpj'],
+          },
+        ],
       });
     } catch (error) {
       throw new HttpError(500, 'Erro ao buscar produtos.', error);
@@ -18,7 +38,23 @@ export default class ProductsModel {
     try {
       const product = await productsModel.findByPk(id, {
         attributes: { exclude: ['category_id', 'brand_id', 'supplier_id'] },
-        include: { all: true },
+        include: [
+          {
+            model: categoriesModel,
+            as: 'category',
+            attributes: ['id', 'name'],
+          },
+          {
+            model: BrandsModel,
+            as: 'brand',
+            attributes: ['id', 'name'],
+          },
+          {
+            model: SuppliersModel,
+            as: 'supplier',
+            attributes: ['id', 'contact_name', 'corporate_name', 'cnpj'],
+          },
+        ],
       });
       if (!product) throw new HttpError(404, 'Produto não encontrado.');
       return product;
