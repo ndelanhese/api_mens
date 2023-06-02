8ac3443c80eeca7aaf87f2cfa189feead9d83e57
ndelanhese <nnathanh3@gmail.com>
2023-06-01 22:32:11 -0300

    feat(promotions): added promottions controller

--- app/api/Promotions/Controllers/PromotionsController.ts
+++ app/api/Promotions/Controllers/PromotionsController.ts
@@ -1,8 +1,15 @@
 import PaginationFactory from '@app/api/Shared/Factories/PaginationFactory';
+import CreatePromotionAction from '@app/src/Promotions/Application/Actions/Promotions/CreatePromotionAction';
+import DeletePromotionAction from '@app/src/Promotions/Application/Actions/Promotions/DeletePromotionAction';
+import UpdatePromotionAction from '@app/src/Promotions/Application/Actions/Promotions/UpdatePromotionAction';
 import BaseController from '@base-controller/BaseController';
 import HttpError from '@exceptions/HttpError';
 import { Request, Response } from 'express';
 
+import CreatePromotionFactory from '../Factories/CreatePromotionFactory';
+import DeletePromotionFactory from '../Factories/DeletePromotionFactory';
+import UpdatePromotionDateFactory from '../Factories/UpdatePromotionDataFactory';
+import UpdatePromotionFactory from '../Factories/UpdatePromotionFactory';
 import PromotionsModel from '../Models/PromotionsModel';
 
 export default class PromotionsController extends BaseController {
@@ -50,6 +57,80 @@ export default class PromotionsController extends BaseController {
       const promotion = await promotionsModel.getPromotion(id);
       await this.createCache(cacheKey, promotion);
       return res.status(200).json(promotion);
+    } catch (error) {
+      if (error instanceof HttpError) {
+        return res.status(error.statusCode).send({ message: error.message });
+      }
+    }
+  }
+
+  public async createPromotion(
+    req: Request,
+    res: Response,
+  ): Promise<Response<string> | undefined> {
+    try {
+      await this.verifyPermission(req, 'promotions_create');
+      const promotionInputData = CreatePromotionFactory.fromRequest(req);
+      const promotionAction = new CreatePromotionAction();
+      const promotionId = (
+        await promotionAction.execute(promotionInputData)
+      ).getId();
+      await this.deleteCache('promotions');
+      return res.status(201).json(promotionId);
+    } catch (error) {
+      if (error instanceof HttpError) {
+        return res.status(error.statusCode).send({ message: error.message });
+      }
+    }
+  }
+
+  public async deletePromotion(
+    req: Request,
+    res: Response,
+  ): Promise<Response<string> | undefined> {
+    try {
+      await this.verifyPermission(req, 'promotions_delete');
+      const promotionInputData = DeletePromotionFactory.fromRequest(req);
+      const promotionAction = new DeletePromotionAction();
+      await promotionAction.execute(promotionInputData);
+      await this.deleteCache('promotions');
+      return res.status(204).send();
+    } catch (error) {
+      if (error instanceof HttpError) {
+        return res.status(error.statusCode).send({ message: error.message });
+      }
+    }
+  }
+
+  public async updatePromotion(
+    req: Request,
+    res: Response,
+  ): Promise<Response<string> | undefined> {
+    try {
+      await this.verifyPermission(req, 'promotions_update');
+      const promotionInputData = UpdatePromotionFactory.fromRequest(req);
+      const promotionAction = new UpdatePromotionAction();
+      await promotionAction.execute(promotionInputData);
+      await this.deleteCache('promotions');
+      return res.status(204).send();
+    } catch (error) {
+      if (error instanceof HttpError) {
+        return res.status(error.statusCode).send({ message: error.message });
+      }
+    }
+  }
+
+  public async updatePromotionDate(
+    req: Request,
+    res: Response,
+  ): Promise<Response<string> | undefined> {
+    try {
+      await this.verifyPermission(req, 'promotions_update');
+      const promotionInputData = UpdatePromotionDateFactory.fromRequest(req);
+      const promotionAction = new UpdatePromotionAction();
+      await promotionAction.execute(promotionInputData);
+      await this.deleteCache('promotions');
+      return res.status(204).send();
     } catch (error) {
       if (error instanceof HttpError) {
         return res.status(error.statusCode).send({ message: error.message });
