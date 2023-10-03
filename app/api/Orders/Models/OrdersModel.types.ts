974d8588ed8b7a58b514e41c6c7de3fb5c4d89d9
ndelanhese <nnathanh3@gmail.com>
2023-10-03 20:18:22 -0300

    feat(orders): added orders types

--- app/api/Orders/Models/OrdersModel.types.ts
+++ app/api/Orders/Models/OrdersModel.types.ts
@@ -5,3 +5,85 @@ export interface IOrderFilter {
   customers_id?: Array<number>;
   users_id?: Array<number>;
 }
+
+interface Customer {
+  id: number;
+  name: string;
+  cpf: string;
+  rg: string;
+  birth_date: string;
+  phone: string;
+  status: string | null;
+  createdAt: string;
+  updatedAt: string;
+}
+
+interface Employee {
+  id: number;
+  name: string;
+  cpf: string;
+  rg: string;
+  birth_date: string;
+  phone: string;
+  pis_pasep: string;
+  admission_date: string;
+  resignation_date: string | null;
+  status: string | null;
+  createdAt: string;
+  updatedAt: string;
+}
+
+interface User {
+  id: number;
+  user: string;
+  email: string;
+  status: string;
+  employee_id: number;
+  deletedAt: string | null;
+  createdAt: string;
+  updatedAt: string;
+  employee: Employee;
+}
+
+interface Product {
+  id: number;
+  part_number: string;
+  name: string;
+  description: string;
+  purchase_price: number;
+  price: number;
+  size: string;
+  color: string;
+  quantity: number;
+  status: string;
+  category_id: number;
+  brand_id: number;
+  supplier_id: number;
+  createdAt: string;
+  updatedAt: string;
+}
+
+interface OrderProduct {
+  id: number;
+  quantity: number;
+  order_id: number;
+  product_id: number;
+  createdAt: string;
+  updatedAt: string;
+  product: Product;
+}
+
+export interface Order {
+  id: number;
+  date: string;
+  description: string;
+  observation: string;
+  status: string;
+  customer_id: number;
+  user_id: number;
+  createdAt: string;
+  updatedAt: string;
+  customer: Customer;
+  user: User;
+  orders_products: OrderProduct[];
+}
